95e3c98cea755594e48c11c17afcf565748daa3e
athirai-s <athiraishanmugam7@gmail.com>
2024-03-29 12:22:34 -0400

    Added files

---
+++ BulkyWebRazor_Tempn/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]  //add data annotation to make it a primary key
+        public int Id { get; set; }
+        [Required] //not null value
+        [MaxLength(30)]
+        //[DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
